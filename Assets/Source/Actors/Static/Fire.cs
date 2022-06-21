715b443130ffd66533a36c93b54bbb2e1172fef7
KisLorand <kislorand270@gmail.com>
2022-06-21 13:21:56 +0200

    add Fire Actor

---
+++ Assets/Source/Actors/Static/Fire.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DungeonCrawl.Core;
+using Assets.Source.Actors.Static;
+using DungeonCrawl.Actors.Characters;
+using DungeonCrawl.Actors.Static;
+
+namespace DungeonCrawl.Actors.Static
+{
+	public class Fire : Actor
+    {
+        public override int DefaultSpriteId => 494;
+        public override string DefaultName => "Fire";
+
+        public override bool Detectable => true;
+
+        public override int Z => -1;
+
+        public override bool OnCollision(Actor anotherActor)
+        {
+            anotherActor.OnCollision(this);
+            return true;
+        }
+
+
+    }
+}
