8adfb64bcd61b7123d2734e1bf018bb914a8eaf9
KisLorand <kislorand270@gmail.com>
2022-06-21 11:46:50 +0200

    add PigBoss base

---
+++ Assets/Source/Actors/Characters/PigBoss.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using DungeonCrawl.Core;
+
+namespace DungeonCrawl.Actors.Characters
+{
+	public class PigBoss : Character
+	{
+		private const int HEALTH = 42;
+		private const int DAMAGE = 10;
+
+		public PigBoss() : base(HEALTH, DAMAGE)
+		{
+		}
+
+		protected override void OnUpdate(float deltaTime)
+		{
+		}
+
+		protected override void OnDeath()
+		{
+			Debug.Log("Pig SLAYED");
+			ActorManager.Singleton.Spawn<PigBeast>(this.Position);
+			Destroy(this);
+		}
+
+		public override int DefaultSpriteId => 413;
+
+		public override string DefaultName => "PigBoss";
+	}
+}
