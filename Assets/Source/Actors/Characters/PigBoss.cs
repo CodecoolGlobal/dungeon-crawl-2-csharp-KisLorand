74bf6b52058f7e21138ccbebd55b8145ae7640f7
KisLorand <kislorand270@gmail.com>
2022-06-22 15:48:15 +0200

    add Lava and Fire Lists to pigBoss

--- Assets/Source/Actors/Characters/PigBoss.cs
+++ Assets/Source/Actors/Characters/PigBoss.cs
@@ -2,6 +2,7 @@
 using DungeonCrawl.Core;
 using DungeonCrawl.Actors.Static;
 using System.Collections.Generic;
+using System;
 
 namespace DungeonCrawl.Actors.Characters
 {
@@ -16,13 +17,21 @@ namespace DungeonCrawl.Actors.Characters
 		private const int ATTACK_LENGTH = 25;
 		private const int ATTACK_RECOVERY = 10;
 
-		private List<Fire> _effects;
-		private List<Wall> _walls;
+		private List<Fire> _fireEffects;
+		private List<Wall> _lavaEffects;
 
 		public PigBoss() : base(HEALTH, DAMAGE)
 		{
-			_effects = new List<Fire>();
+			_fireEffects = new List<Fire>();
 		}
+		public override int DefaultSpriteId => 413;
+		public override string DefaultName => "PigBoss";
+		protected override void OnDeath()
+		{
+			Debug.Log("Pig SLAYED");
+			Destroy(this);
+		}
+
 
 		protected override void OnUpdate(float deltaTime)
 		{
@@ -31,50 +40,67 @@ namespace DungeonCrawl.Actors.Characters
 			if (HIT == ATTACK_START)
 			{
 				//windup
-				Wall wall;
+				Wall lava;
 				for (int i = 0; i < firePositions.Count; i++)
 				{
-					wall = ActorManager.Singleton.Spawn<Wall>(firePositions[i]);
-					_walls.Add(wall);
+					lava = ActorManager.Singleton.Spawn<Wall>(firePositions[i]);
+					_lavaEffects.Add(lava);
 				}
 				//windup
 			}
 			else if (HIT == ATTACK_START + ATTACK_WINDUP)
 			{
 				//spawnEffect(T effect, List<(int x, int y)> positions)
-				Fire fire;
-				for (int i = 0; i < firePositions.Count; i++)
-				{
-					fire = ActorManager.Singleton.Spawn<Fire>(firePositions[i]);
-					_effects.Add(fire);
-				}
+				SpawnEffect(firePositions);
 				//spawn fire
 			}
 			else if (HIT == ATTACK_START + ATTACK_WINDUP + ATTACK_LENGTH)
 			{
-				for(int i=0; i<_effects.Count; i++)
+				for(int i=0; i< _fireEffects.Count; i++)
 				{
-					ActorManager.Singleton.DestroyActor(_effects[i]);
-					_effects[i].PutOut();
-					_effects[i] = null;
+					Debug.Log(_fireEffects[i]);
+					ActorManager.Singleton.DestroyActor(_fireEffects[i]);
+					_fireEffects[i].PutOut();
+					_fireEffects[i] = null;
 				}
-				_effects = new List<Fire>();
+				_fireEffects = new List<Fire>();
 			}
 			else if (HIT == ATTACK_START + ATTACK_WINDUP + ATTACK_LENGTH + ATTACK_RECOVERY)
 			{
 				//recovery
 				Wall[] wallCopy = new Wall[8];
-				_walls.CopyTo(wallCopy);
+				_lavaEffects.CopyTo(wallCopy);
 				for (int i = 0; i < wallCopy.Length; i++)
 				{
-					_walls.Remove(_walls[i]);
-					ActorManager.Singleton.DestroyActor(_walls[i]);
+					_lavaEffects.Remove(_lavaEffects[i]);
+					ActorManager.Singleton.DestroyActor(_lavaEffects[i]);
 				}
 				//recovery
 				HIT = 0;
 			}
 			HIT++;
 		}
+
+		private void SpawnEffect(List<(int x, int y)> firePositions)
+		{
+			Fire effect;
+			for (int i = 0; i < firePositions.Count; i++)
+			{
+				effect = ActorManager.Singleton.Spawn<Fire>(firePositions[i]);
+				_fireEffects.Add(effect);
+			}
+		}
+
+
+		/*private void SpawnEffect<T>(List<(int x, int y)> firePositions) where T : Actor
+		{
+			T effect;
+			for (int i = 0; i < firePositions.Count; i++)
+			{
+				effect = ActorManager.Singleton.Spawn<T>(firePositions[i]);
+				_effects.Add(effect);
+			}
+		}*/
 
 		private List<(int x, int y)> GetAdjPositions()
 		{
@@ -94,14 +120,5 @@ namespace DungeonCrawl.Actors.Characters
 			return adjPositions;
 		}
 
-		protected override void OnDeath()
-		{
-			Debug.Log("Pig SLAYED");
-			Destroy(this);
-		}
-
-		public override int DefaultSpriteId => 413;
-
-		public override string DefaultName => "PigBoss";
 	}
 }
