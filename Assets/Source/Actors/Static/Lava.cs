c7e22fe66c0c3276da1279d5bd3ca2501d8356aa
Imreszab <szabimi12@gmail.com>
2022-06-22 14:07:46 +0200

    Created Lava Class

---
+++ Assets/Source/Actors/Static/Lava.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DungeonCrawl.Actors.Static
+{
+    internal class Lava :Actor
+    { 
+        public override int DefaultSpriteId => 101;
+        public override string DefaultName => "Lava";
+        public override bool Detectable => false;
+    }
+}
